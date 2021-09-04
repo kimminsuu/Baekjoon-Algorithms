ea014076b52dfb556720ec3f8056b52cce504a1d
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-09-04 23:39:16 +0900

    Add files via upload

--- 13460.py
+++ 13460.py
@@ -20,9 +20,22 @@ def bfs() :
         for i in range(4) :
             nx = x+dx[i]
             ny = y+dy[i]
-            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == '.' :
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == '.' and check[nx][ny] == 0:
                 while graph[nx][ny] == '.' :
                     check[nx][ny] = check[x][y] + 1
                     nx += dx[i]
                     ny += dy[i]
+                if graph[nx][ny] == 'O' :
+                    print(check[x][y]+1)
+                    return
+            if graph[nx][ny] == 'O' :
+                print(check[x][y]+1)
+                return
+
+for i in range(n) :
+    for j in range(m) :
+        if check[i][j] == 'R' :
+            queue1.append((i,j))
+            bfs()
+            return
     
