9d595840171db8da9c3e4ed0ac3a0e264ba03280
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-09-28 23:26:44 +0900

    Add files via upload

---
+++ 2512.py
@@ -0,0 +1,22 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+money = list(map(int,read().split()))
+m = int(read())
+money.sort()
+
+start, end = money[0], money[-1]
+while start <= end :
+    mid = (start+end) // 2
+    val = 0
+    for i in range(n) :
+        if money[i] > mid :
+            val += mid
+        else :
+            val += money[i]
+    if val > m :
+        end = mid-1
+    else :
+        start = mid + 1
+print(end)
