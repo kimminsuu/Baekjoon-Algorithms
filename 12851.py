9021ab2b43854d8624a19c14ad07fdb67aadf883
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-10-19 23:10:19 +0900

    Add files via upload

--- 12851.py
+++ 12851.py
@@ -7,10 +7,17 @@ queue = deque()
 queue.append(n)
 
 count = 0
-check = [0] * 100001
+check = [-1] * 100001
 check[n] = 1
 while queue :
     x = queue.popleft()
     if x == k :
         count+=1
+    for i in [x+1, x-1, 2*x] :
+        if 0<=i<100001 :
+            if check[i]==-1 or check[i]>=check[x]+1 :
+                check[i] = check[x]+1
+                queue.append(i)
+print(check[k])
+print(count)
     
