69db2bd71ad883d976513df28632216c716fdb1c
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-11-08 22:06:45 +0900

    Add files via upload

--- 2579.py
+++ 2579.py
@@ -2,17 +2,16 @@ import sys
 read = sys.stdin.readline
 
 n = int(read())
+s = [0] * 301
+arr = [0]*301
 
-s = [0 for i in range(301)]
-dp = [0 for i in range(301)]
-
-for i in range(n):
+for i in range(n) :
     s[i] = int(read())
-dp[0] = s[0]
-dp[1] = s[0] + s[1]
-dp[2] = max(s[1] + s[2], s[0] + s[2])
 
-for i in range(3, n):
-    dp[i] = max(dp[i - 3] + s[i - 1] + s[i], dp[i - 2] + s[i])
-    
-print(dp[n - 1])
+arr[0] = s[0]
+arr[1] = s[0] + s[1]
+arr[2] = max(s[0] + s[2] , s[1] + s[2])
+
+for i in range(3,n) :
+    arr[i] = max(arr[i-3]+s[i-1]+s[i], arr[i-2]+s[i])
+print(arr[n-1])
