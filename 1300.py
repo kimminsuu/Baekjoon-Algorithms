b816af57abcb067f58a7354cfcb3b4ba3da6a06f
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-11-18 23:41:43 +0900

    Add files via upload

---
+++ 1300.py
@@ -0,0 +1,15 @@
+n,k = map(int,input().split())
+start,end = 1,k
+
+while start <= end :
+    mid = (start+end) // 2
+    temp = 0
+    for i in range(1,n+1) :
+        temp += min(mid//i,n)
+
+    if temp >=k :
+        ans = mid
+        end = mid-1
+    else :
+        start = mid+1
+print(ans)
